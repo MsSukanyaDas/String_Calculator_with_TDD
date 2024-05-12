4e278c3130885e369090eeeb452c57ddd8a959df
Sukanya Das <sukanya15das@gmail.com>
2024-05-12 13:35:14 +0530

    Functionality for delimiters, line break and negative numbers; code coverage added

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -13,7 +13,24 @@ describe("Add function", () => {
   it("returns sum when input 5 numbers", () => {
     expect(add("8,2,5,7,0")).toBe(22);
   });
-  it.todo("handles new lines");
-  it.todo("supports different delimiters");
-  it.todo("throws error for negative number");
+  it("handles new lines", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+  it("supports different delimiter ;", () => {
+    expect(add("//;\n1;2;8;9")).toBe(20);
+  });
+  it("supports different delimiter -", () => {
+    expect(add("//-\n1-2-8-9")).toBe(20);
+  });
+  it("throws error & prints them for negative numbers", () => {
+    expect(() => add("-5,2,-3,10")).toThrow(
+      "negative numbers not allowed -5,-3,"
+    );
+  });
+  it("throws invalid input error with extra delimiter", () => {
+    expect(() => add("//-\n1-2-8--9-")).toThrow("Invalid Input");
+  });
+  it("throws invalid input error with extra line break", () => {
+    expect(() => add("1,\n")).toThrow("Invalid Input");
+  });
 });
