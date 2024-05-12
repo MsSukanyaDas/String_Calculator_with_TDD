f3406a5f30ed8cee0389dc477244a0471e2a2ae9
Sukanya Das <sukanya15das@gmail.com>
2024-05-12 11:58:01 +0530

    Any amount of numbers handles & some todo tests

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,12 @@
-export const add = function (stringNumbers) {
-  //code for string calculator
+const add = function (stringNumbers) {
+  let result = 0;
+  if (stringNumbers.length <= 1) result = Number(stringNumbers);
+  else {
+    result = stringNumbers.split(",").reduce((accumulator, item) => {
+      return (accumulator += Number(item));
+    }, 0);
+  }
+  return result;
 };
+
+module.exports = add;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,5 +1,19 @@
+add = require("../src/calculator");
+
 describe("Add function", () => {
-  it.todo("returns 0 when empty string");
-  it.todo("returns number when input is one number");
-  it.todo("returns sum when input more than one number");
+  it("returns 0 when empty string", () => {
+    expect(add("")).toBe(0);
+  });
+  it("returns number when input is one number", () => {
+    expect(add("8")).toBe(8);
+  });
+  it("returns sum when input more than one number", () => {
+    expect(add("8,2")).toBe(10);
+  });
+  it("returns sum when input 5 numbers", () => {
+    expect(add("8,2,5,7,0")).toBe(22);
+  });
+  it.todo("handles new lines");
+  it.todo("supports different delimiters");
+  it.todo("throws error for negative number");
 });
